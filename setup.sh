c9139eb0b27b3fb53d9599137bb0640eb1925e10
Anubhav Kashyap <61369802+anubhavanonymous@users.noreply.github.com>
2021-02-21 20:55:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ apt update && apt upgrade -y
 apt install figlet -y
 apt install python2 -y
 apt install python -y
-figlet -f Installing..
+figlet -c -f slant Installing..
 apt install toilet
 apt install wget -y
 pip2 install requests
